8000a57f9b3f361f72f2f03c46bebe5036af27a9
Hesham AbdAllah <zarboxa@gmail.com>
2017-08-11 07:55:35 +0200

    TimeDoctor test for front-end developer.

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports=function(grunt){
+
+
+	grunt.initConfig({
+
+		meta : {
+			banner : '/* AngularJs Dependencies <%= grunt.template.today() %> */'
+		},
+
+		uglify: {
+			jsLib: {
+				src: [
+					'node_modules/jquery/dist/jquery.min.js',
+					'node_modules/bootstrap/dist/js/bootstrap.min.js',
+					'node_modules/angular/angular.min.js',
+					'node_modules/angular-animate/angular-animate.min.js',
+					'node_modules/angular-route/angular-route.min.js',
+					'node_modules/angular-sanitize/angular-sanitize.min.js',
+					'node_modules/angular-aria/angular-aria.min.js',
+					'node_modules/angular-messages/angular-messages.min.js',
+					'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+					'node_modules/highcharts/highcharts.js',
+					'node_modules/highcharts/highcharts-more.js',
+					'node_modules/highcharts/modules/solid-gauge.js',
+					'node_modules/highcharts-ng/dist/highcharts-ng.min.js',
+					],
+				dest: 'js/dep.min.js',
+			},
+			dist: {
+				src: ['js/scripts.js'],
+				dest: 'js/scripts.min.js'
+			},
+		},
+		cssmin: {
+			minify: {
+				src: [
+					'node_modules/bootstrap/dist/css/bootstrap.min.css',
+					'node_modules/components-font-awesome/css/font-awesome.min.css',
+					'css/styles.css'
+				],
+				dest: 'css/styles.min.css'
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true, 
+						src: [
+							'node_modules/bootstrap/fonts/*',
+							'node_modules/components-font-awesome/fonts/*'
+						],
+						dest: 'fonts',
+						filter: 'isFile',
+						flatten:true
+					},
+				],
+			}
+		},
+	    serve: {
+			options: {
+				port: 9000,
+				'aliases':{
+					'index': {
+						tasks: ['uglify'],
+						output: 'index.html'
+					}
+				}
+			}
+	    }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-serve');
+
+
+	grunt.registerTask('default',['cssmin','copy','uglify','serve']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # timedoctor
+
+Please run `npm install` to download neccessary dependencies.
+
+Then run `grunt` to construct the app.
+
+Then open : http://localhost:9000/index.html
